a86abc6a7f5295bd89158677943fe960edf9ed86
Phan Văn Lũy <Vluybb@gmail.com>
2021-11-17 11:52:57 +0700

    finish

---
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/util/UploadFileUtils.java
@@ -0,0 +1,25 @@
+package com.laptrinhjavaweb.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UploadFileUtils {
+	public final String root = "E:\\Java Web Workspace\\webchotot\\spring-mvc\\src\\main\\webapp\\template\\admin";
+		
+	    public void writeOrUpdate(byte[] bytes, String path) {
+	        File file = new File(StringUtils.substringBeforeLast(root + path, "/"));
+	        if (!file.exists()) {
+	            file.mkdir();
+	        }
+	        try(FileOutputStream fileOutputStream = new FileOutputStream(new File(root + path))) {
+	            fileOutputStream.write(bytes);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+}
